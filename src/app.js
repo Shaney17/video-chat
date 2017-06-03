ac1c526265611c5f391e655f702997cc9f9e8b75
Shaney17 <westlife112@gmail.com>
2017-06-03 18:33:00 +0700

    add connect and send

--- src/app.js
+++ src/app.js
@@ -1,4 +1,26 @@
-const openCamera = require("./openCamera");
+const openStream = require("./openStream");
 
-openCamera();
-console.log("Hello !");
+// openStream();
+
+const Peer = require('simple-peer');
+
+const p = new Peer({ initiator: location.hash === "#1", trickle: false });
+
+p.on('signal', function(token){
+    $('#txtMySignal').val(JSON.stringify(token));
+});
+
+p.on('connect', function(){
+    setInterval(function(){
+        p.send(Math.random());
+    }, 2000);
+});
+
+p.on('data', function(data){
+    console.log(data);
+});
+
+$("#btnConnect").click(function(){
+    const friendSignal = JSON.parse($('#txtFriendSignal').val());
+    p.signal(friendSignal);
+});
